aac9e5d958e661f91284df90fb91b0dce7345404
Estrella <gemilangtirto2002@gmail.com>
2024-06-09 11:12:39 +0700

    1

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -5,12 +5,13 @@
             <div class="card-body">
                 <h1 class="card-title">{{ $blog->title }}</h1>
                 <p class="text-muted">By: {{ $blog->user->name }}</p>
-                <p class="card-text">{{ $blog->desc }}</p>
+                <p class="card-text">{!! $blog->desc !!}</p>
                 @if ($blog->image_path)
                     <div class="mb-3">
                         <img src="{{ asset('storage/' . $blog->image_path) }}" alt="Blog Image" class="img-fluid" style="max-width: 50%; height: auto;">
                     </div>
                 @endif
+                <br>
                 <div class="d-flex justify-content-start">
                     <a href="{{ route('blogs.edit', $blog->id) }}" class="btn btn-primary mr-2">Edit</a>
                     <form action="{{ route('blogs.destroy', $blog->id) }}" method="POST" style="display: inline-block;">
